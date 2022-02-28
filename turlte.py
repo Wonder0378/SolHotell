1633ddd748a1466a53981b1bec12cfa649284a5b
SamuelHellqvist <samuel@hellqvist.nu>
2022-02-28 09:51:07 +0100

    new turtle change

---
+++ turlte.py
@@ -0,0 +1,25 @@
+import turtle
+from turtle import *
+
+#preperation
+width(7)
+speed(5)
+bgcolor("white")
+pencolor("black")
+up()
+backward(250)
+left(90)
+forward(200)
+left(180)
+
+#H
+
+#E
+
+#J
+
+#D
+
+#Å
+
+done()
