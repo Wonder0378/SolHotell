c4bf904bf9dd868d359de5e7c1b8b28bbef1e54e
SamuelHellqvist <samuel@hellqvist.nu>
2022-03-14 14:02:26 +0100

    some changes regarding colors

--- newktwindow.py
+++ newktwindow.py
@@ -1,14 +1,12 @@
-
-
-
 """
-Name: Samuel Hellqvist
+Name: Samuel Hellqvist and Marcus Hedquist
 Date: 09-03-2022
 Info:
-<new file for the hotel
+new file for the hotel
 everything is given name like this:
-self.whatthisis_what-type-of-tkinter-element-thtis-is
->
+
+self.what-this-is_what-type-of-tkinter-element-thtis-is
+
 """
 
 #importing everythinig from tkinter 
@@ -25,6 +23,9 @@ class Main():
         #defining the pic that will later be the background image
         self.pic = PhotoImage(file="SolHotell/pictures/finstrand.png")
 
+        self.carrot = "#ff8e00"
+        self.blue = "#003f7d"
+
         self.text = tk.StringVar()
         self.text.set("")
 
@@ -36,21 +37,21 @@ class Main():
         self.bg_label.place(x=0, y=0, relheight=1, relwidth=1)
 
         #creating the label with the headline
-        self.header_label = tk.Label(self.root, text="☀" + "Sammys" + " "*3 + "solhotell" + "☀", fg="orange", font=("Broadway", 50), bg='medium blue')
+        self.header_label = tk.Label(self.root, text="☀" + "Sammys" + " "*3 + "solhotell" + "☀", bg=self.blue, fg=self.carrot, font=("Broadway", 50))
 
         #creating the exit button, this button will have the command escape
-        self.exit_button = tk.Button(self.root, text="exit", command=self.escape, fg="red", bg="white", width=10)
+        self.exit_button = tk.Button(self.root, text="EXIT", command=self.escape, width=10, bg=self.carrot, fg="black")
 
         #creating a button wich will show all the rooms
-        self.room_button = tk.Button(self.root, text="UPPTÄCK VÅRA RUM", width=20, command=self.view, bg="orange", fg="white", font="Arial")
+        self.roominfo_button = tk.Button(self.root, text="UPPTÄCK VÅRA RUM", width=20, command=self.view, bg=self.carrot, fg="white", font="Arial")
 
         #creating a button that will let the user check out
-        self.checkout_button = tk.Button(self.root, text="CHECKA UT", width=20, command=self.checkout, bg="orange", fg="white", font="Arial")
+        self.checkout_button = tk.Button(self.root, text="CHECKA UT", width=20, command=self.checkout, bg=self.carrot, fg="white", font="Arial")
 
         #packing all the tkinter elements and palcing them
         self.header_label.place(relx=0.5, rely=0.3, anchor=CENTER)
         self.exit_button.place(relx=0.9, rely=0.9)
-        self.room_button.place(relx=0.2, rely=0.5)
+        self.roominfo_button.place(relx=0.2, rely=0.5)
         self.checkout_button.place(relx=0.2, rely=0.6)
 
     #function that makes the program run
@@ -64,26 +65,26 @@ class Main():
     #funtion to view all different rooms
     def view(self):
         #creating the frame that will contain infromation about the room
-        self.information_frame = tk.Frame(self.root, background="yellow")
+        self.information_frame = tk.Frame(self.root, background="#E7E0DB")
 
         #creating the label that will show information about the deluxeroom
-        self.roominformation_label = tk.Label(self.information_frame, textvariable=self.text, font=("Arial", 10))
+        self.roominformation_label = tk.Label(self.information_frame, textvariable=self.text, font=("Arial", 10), bg="#E7E0DB")
 
         #creating a button that will be used to show information about the luxurious room
-        self.deluxeroom_info_button = tk.Button(self.information_frame, text="Deluxerum", command=self.deluxe_info, padx=30, font=("Arial", 12))
+        self.deluxeroom_info_button = tk.Button(self.information_frame, text="Deluxerum", command=self.deluxe_info, padx=30, font=("Arial", 12), bg=self.carrot, fg="white")
         #this bit of code lets ut hide the button until we need it
         self.deluxeroom_info_button.pack_forget()
 
         #creating a button that will display informatoin about the standard room
-        self.economyroom_info_button = tk.Button(self.information_frame, text="Standardrum", command=self.standard_info, padx=30, font=("Arial", 12))
+        self.economyroom_info_button = tk.Button(self.information_frame, text="Standardrum", command=self.standard_info, padx=30, font=("Arial", 12), bg=self.carrot, fg="white")
         self.economyroom_info_button.pack_forget()
         
         #creating a button that will display information about the family room
-        self.familyroom_info_button = tk.Button(self.information_frame, text="Familjerum", command=self.family_info, padx=30, font=("Arial", 12))
+        self.familyroom_info_button = tk.Button(self.information_frame, text="Familjerum", command=self.family_info, padx=30, font=("Arial", 12), bg=self.carrot, fg="white")
         self.familyroom_info_button.pack_forget()
 
         #creating a button that will be closing the information frame
-        self.closeinfoframe_button = tk.Button(self.information_frame, text="x", command=self.closeinfoframe, fg=("red"), font=("Arial", 12))
+        self.closeinfoframe_button = tk.Button(self.information_frame, text="x", command=self.closeinfoframe, fg="red", font=("Arial", 12))
         self.closeinfoframe_button.pack_forget()
 
         #creating the information frame and packing everythin
@@ -98,6 +99,15 @@ class Main():
     #function
     def checkout(self):
         pass
+    
+    def standard_info(self):
+        #opening the file that will contain the information we want to show
+        f=open('SolHotell/z_standardroom.txt', encoding='utf-8')
+        newtext = "\n" + f.read() + "\n"
+        self.text.set(newtext)
+        #creating and adding a button that will be used for booking a room
+        self.book_button = tk.Button(self.information_frame, text="Boka", command=self.bookroom, font=("Arial"), bg=self.carrot, fg="white")
+        self.book_button.grid(row=3, column=1)
 
     #function for viewing information about the deluxe room
     def deluxe_info(self):
@@ -106,25 +116,17 @@ class Main():
         newtext = "\n" + f.read() + "\n"
         self.text.set(newtext)
         #creating and adding a button that will be used for booking a room
-        self.book_button = tk.Button(self.information_frame, text="Boka", command=self.bookroom, font=("Arial"))
-        self.book_button.grid(row=3, column=1)
-    
-    def standard_info(self):
-        #opening the file that will contain the information we want to show
-        f=open('SolHotell/z_standardroom.txt', encoding='utf-8')
-        newtext = "\n" + f.read() + "\n"
-        self.text.set(newtext)
-        #creating and adding a button that will be used for booking a room
-        self.book_button = tk.Button(self.information_frame, text="Boka", command=self.bookroom, font=("Arial"))
+        self.book_button = tk.Button(self.information_frame, text="Boka", command=self.bookroom, font=("Arial"), bg=self.carrot, fg="white")
         self.book_button.grid(row=3, column=1)
 
+    #function for viewing information about the family room
     def family_info(self):
         #creating and adding a button that will be used for booking a room
         f=open('SolHotell/z_familyroom.txt', encoding='utf-8')
         newtext = "\n" + f.read() + "\n"
         self.text.set(newtext)
         #creating and adding a button that will be used for booking a room
-        self.book_button = tk.Button(self.information_frame, text="Boka", command=self.bookroom, font=("Arial"))
+        self.book_button = tk.Button(self.information_frame, text="Boka", command=self.bookroom, font=("Arial"), bg=self.carrot, fg="white")
         self.book_button.grid(row=3, column=1)
 
     #funtion that will close the information frame
