e932898d8fd4f220c9ddca1aaef1841c2dc00c27
Wonder0378 <marcusinskolan@gmail.com>
2022-03-21 08:50:09 +0100

    s

--- tkwindow.py
+++ tkwindow.py
@@ -9,18 +9,12 @@ import tkinter.messagebox as msgbox
 root=Tk()
 from turtle import *
 class Main:
-<<<<<<< HEAD
-    #pic = PhotoImage(file="pictures/mallorca.png")
-=======
->>>>>>> 2d8c1930e3a69ffde1bd97748d7322195de3ed28
     def check():
         msgbox.showinfo("Empty", "Rummet bokat!")
     root.geometry("850x500")
-<<<<<<< HEAD
+
     my_label=Label(root, bg="blue")
     my_label.place(x=0, y=0, relheight=1, relwidth=1)
-=======
->>>>>>> 2d8c1930e3a69ffde1bd97748d7322195de3ed28
 
     my_headline = Label(root, text="Sammy's solhotell",fg="orange", font=("Helvetica", 40))
     my_headline.place(relx=0.5, rely=.3, anchor=CENTER)
