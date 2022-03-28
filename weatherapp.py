0d09606f5f0a90f3b27e47873ae697f1928c2d6a
SamuelHellqvist <samuel@hellqvist.nu>
2022-03-28 09:53:35 +0200

    weather app built. Thank you youtube and gabriel

--- weatherapp.py
+++ weatherapp.py
@@ -5,14 +5,20 @@ Info:
 This is a weather app that will display what weather it is somewhere in the world
 """
 
-"""
 import requests
 
-api_key = ''
+api_key = '647606470e7d8926da64ece5273d596b'
 
 user_input = input("Enter city: ")
 
-weather_data = request.get(
-    f""
-)
-"""
+weather_data = requests.get(
+     f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+weather = weather_data.json()['weather'][0]['main']
+temp = weather_data.json()['main']['temp']
+
+temp2 = temp-32
+tempCelcius = round(temp2*0.5556)
+
+print(f"The weather in {user_input} is: {weather}")
+print(f"The temperature in {user_input} is: {tempCelcius}°C")
