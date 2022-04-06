c5c39b64f0ad067a1fdb7d86d5b3ca2b424064d5
SamuelHellqvist <samuel@hellqvist.nu>
2022-04-06 12:57:44 +0200

    send

--- newktwindow.py
+++ newktwindow.py
@@ -13,7 +13,6 @@ self.what-this-is_what-type-of-tkinter-element-thtis-is
 import tkinter as tk
 from tkinter import *
 from bookunbook import Room
-from getweather import GetWeather
 from random import randint
 """
 Name: Samuel Hellqvist
@@ -22,22 +21,47 @@ Info:
 This is a weather app that will display what weather it is somewhere in the world
 """
 
-#hela programmet kommer att skriva under denna main class
+#everything will be udner this main class
 class Main():
     def __init__(self):
         self.__roomids = []
         self.getrooms()
 
         self.title = "Solhotellet, Marcus och Samuel"
-        weather = GetWeather()
 
-        #root, där alltig kommer sättas
+        import requests
+
+        #api-nyckeln används för att få tillgång till data om vädret från hela världen
+        api_key = '647606470e7d8926da64ece5273d596b'
+
+        #vårat hotell ligger i staden Victoria på Sychellerna och därför är inputen Victoria
+        input_ = ("Victoria")
+
+        #data om vädret i Victoria hämtas
+        weather_data = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={input_}&units=imperial&APPID={api_key}")
+
+        #variabler sätts
+        self.weather = weather_data.json()['weather'][0]['main']
+        self.temp = weather_data.json()['main']['temp']
+
+        #variablerna är annars på engelska men översätts här till svenska
+        if self.weather == ("Clear"):
+            self.weather = ("Klart")
+        elif self.weather == ("Clouds"):
+            self.weather = ("Molnigt")
+
+        #temperaturen räknas om från fahrenheit till celcius
+        self.temp2 = self.temp-32
+        self.tempCelcius = round(self.temp2*0.5556)
+        self.tempText = str(self.tempCelcius)
+
+        #creating the root where everything will be placed
         self.root = tk.Tk()
 
-        #sätter pic till en bild
+        #defining the pic that will later be the background image
         self.pic = PhotoImage(file="SolHotell/pictures/finstrand.png")
 
-        #sätter olika frägvariabler som kommer att användas senare
         self.carrot = "#ff8e00" #lightest orange
         self.orange = "#fd7702" #medium orange
         self.mandarin = "#ff5003" #darkest orange
@@ -47,33 +71,33 @@ class Main():
 
         self.white = "#ebebeb" #white/grey
 
-        #text variabel
+        #text variable
         self.text = tk.StringVar()
         self.text.set("")
 
-        #sätter skärmen till fullskärm
+        #making sure that the tkinter window will be in fullscreen
         self.root.attributes("-fullscreen", True)
 
-        #sätter en label som blir bakgrundsbilden
+        #creating the background label that will make the background to the image
         self.bg_label = tk.Label(self.root, image=self.pic)
         self.bg_label.place(x=0, y=0, relheight=1, relwidth=1)
 
-        #skapar en headline
+        #creating the label with the headline
         self.header_label = tk.Label(self.root, text="☀" + "SAMMYS" + " "*3 + "SOLHOTELL" + "☀", bg=self.blue, fg=self.carrot, font=("Broadway", 50))
 
         #skapar en widget där hotellets nuvarande väder skrivs ut
-        self.weather_label = tk.Label(self.root, text="Vädret på hotellet just nu: \n" + weather.tempText + "°C" + "\n" + weather.weather, bg=self.sky, fg=self.white, font=("Arial, 11"))
+        self.weather_label = tk.Label(self.root, text="Vädret på hotellet just nu: \n" + self.tempText + "°C" + "\n" + self.weather, bg=self.sky, fg=self.white, font=("Arial, 11"))
 
-        #skapar en exit-knapp som kommer ha command self.escape
+        #creating the exit button, this button will have the command escape
         self.exit_button = tk.Button(self.root, text="EXIT", command=self.escape, width=10, bg=self.carrot, fg="black")
 
-        #skapar en knapp som visar alla rum
+        #creating a button wich will show all the rooms
         self.roominfo_button = tk.Button(self.root, text="CHECKA IN", width=20, command=self.view, bg=self.carrot, fg=self.white, font="Arial")
 
-        #skapar en knapp som kommer användas när man vill chekca ut
+        #creating a button that will let the user check out
         self.checkout_button = tk.Button(self.root, text="CHECKA UT", width=20, command=self.checkout, bg=self.carrot, fg=self.white, font="Arial")
 
-        #placerar alla tkinter element
+        #packing all the tkinter elements and palcing them
         self.header_label.place(relx=0.5, rely=0.17, anchor=CENTER)
         self.weather_label.place(relx=0.145, rely=0.3)
         self.exit_button.place(relx=0.9, rely=0.9)
@@ -136,38 +160,40 @@ class Main():
                 i.register()
         
 
-    #funktion som gör att programmet körs
+    #function that makes the program run
     def run(self):
         self.root.mainloop()
 
-    #funktion som stänger programmet genom att ta bort rooten
+    #function that closes the tkinter window
     def escape(self):
         self.root.destroy()
 
-    #funktion för att se alla rummen
+    #funtion to view all different rooms
     def view(self):
-        #skapar en frames
+        #creating the frame that will contain infromation about the room
         self.information_frame = tk.Frame(self.root, background=self.white)
+
+        #creating the label that will show information about the deluxeroom
         self.roominformation_frame = tk.Frame(self.information_frame, background="#E7E0DB")
 
-        #en knapp som kommer att visa information om standard-rummet
+        #creating a button that will display informatoin about the standard room
         self.economyroom_info_button = tk.Button(self.information_frame, text="Standardrum", command=self.standard_info, padx=30, font=("Arial", 12), bg=self.sky, fg=self.white)
         self.economyroom_info_button.pack_forget()
 
-        #en knapp som kommer att visa information om lyx-rummet
+        #creating a button that will be used to show information about the luxurious room
         self.deluxeroom_info_button = tk.Button(self.information_frame, text="Deluxerum", command=self.deluxe_info, padx=30, font=("Arial", 12), bg=self.sky, fg=self.white)
         #this bit of code lets ut hide the button until we need it
         self.deluxeroom_info_button.pack_forget()
         
-        #en knapp som kommer att visa information om familje-rummet
+        #creating a button that will display information about the family room
         self.familyroom_info_button = tk.Button(self.information_frame, text="Familjerum", command=self.family_info, padx=30, font=("Arial", 12), bg=self.sky, fg=self.white)
         self.familyroom_info_button.pack_forget()
 
-        #en knapp som stänger new informations-rutan
+        #creating a button that will be closing the information frame
         self.closeinfoframe_button = tk.Button(self.information_frame, text="x", command=self.closeinfoframe, fg="red", font=("Arial", 12))
         self.closeinfoframe_button.pack_forget()
 
-        #packar och placerar alla emelent
+        #creating the information frame and packing everythin
         self.information_frame.place(relx=0.4, rely=0.5)
         self.economyroom_info_button.grid(row=0, column=0)
         self.deluxeroom_info_button.grid(row=0, column=1)
@@ -176,11 +202,11 @@ class Main():
         self.roominformation_frame.grid(row=1, column=0, columnspan=5, sticky="w")
         self.closeinfoframe_button.grid(row=0, column=3)
 
-    #checka-ut funktion
+    #function for checkout
     def checkout(self):
         pass
     
-    #funktion för att visa information om standard-rummet
+    #function for viewing information about the standard room
     def standard_info(self):
         #Placing the rooms
         for index, i in enumerate(self.__standardrooms):
@@ -196,7 +222,7 @@ class Main():
 
 
 
-    #funktion för att visa information om familje-rummet
+    #function for viewing information about the family room
     def family_info(self):
         #creating and adding a button that will be used for booking a room
         for index, i in enumerate(self.__familyrooms):
@@ -210,8 +236,9 @@ class Main():
         except:
             pass
 
-    #funktion för att visa information om lyx-rummet
+    #function for viewing information about the deluxe room
     def deluxe_info(self):
+        #opening the file that will contain the information we want to show
         for index, i in enumerate(self.__deluxerooms):
             i.appear(self.roominformation_frame, (index+1), "deluxe")
 
@@ -223,14 +250,16 @@ class Main():
         except:
             pass
 
-    #funktion som kommer att stänga informations-rutan
+    #funtion that will close the information frame
     def closeinfoframe(self):
+        #the information frame will be destroyed, it will be created again when the show information button will be clicked
         self.information_frame.destroy()
 
-    #funktion för att boka ett rum
+    #function that books the room
     def bookroom(self):
         pass
 
-#kör tkinter-förnstret
+#setting 'app' to equal main
 app=Main()
+#using the run funciton on app
 app.run()
